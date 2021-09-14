bd1710c4f12b91fa3932f44b2a9e82b27ab03762
Oliknud <69842780+Oliknud@users.noreply.github.com>
2021-09-14 19:12:02 +0200

    .

---
+++ css-frameworks-ca/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './scss/styles.scss';
+import Nav from './components/Nav.js';
+
+function App() {
+  return (
+    <div className="App">
+      <Nav />
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ css-frameworks-ca/src/components/Nav.js
@@ -0,0 +1,35 @@
+import React from "react";
+import Navbar from "react-bootstrap/Navbar";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import FormControl from "react-bootstrap/FormControl";
+
+const navBar = () => {
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container className="nav-container">
+        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#link">Link</Nav.Link>
+            <Form className="d-flex">
+              <FormControl
+                type="search"
+                placeholder="Search"
+                className="mr-2"
+                aria-label="Search"
+              />
+              <Button variant="outline-success">Search</Button>
+            </Form>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+};
+
+export default navBar;
